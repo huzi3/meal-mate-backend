304eef9768614fe04ed1ff0234bf4d463c24ab01
huzi3 <135357211+huzi3@users.noreply.github.com>
2025-10-16 20:28:56 +0100

    Add requirements.txt and Procfile for Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MealMate.wsgi
--- meal_mate/settings.py
+++ meal_mate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_i8n8n)-9)xw4-i3c(&yz4=73goow0q74ec0w+gacz*qywhsek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
