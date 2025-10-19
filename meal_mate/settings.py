d94e526e40414d4f58d53554ed1569701a991036
huzi3 <135357211+huzi3@users.noreply.github.com>
2025-10-19 20:27:58 +0100

    Add CORS support and update requirements

--- meal_mate/settings.py
+++ meal_mate/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'meal_mate.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 TEMPLATES = [
     {
